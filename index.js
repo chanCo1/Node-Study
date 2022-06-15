b251aaee9793aeeee4febbc93d1e2d1390bc9be8
chanCo <loinsective@naver.com>
2022-06-15 18:50:34 +0900

    오류해결

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => res.send('Hello World!! 잘 부탁한다!'));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://chanco:abcd1234@cluster.h1gcy.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => console.log('Success!'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello World!!'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
