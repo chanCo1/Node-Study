9876738df0f3dc2d7ad7742847514d79c923d5e8
chanCo <loinsective@naver.com>
2022-06-15 23:09:39 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.post('/register', (req, res) => {
   const user = new User(req.body);
 
   user.save((err, userInfo) => {
-    if(err) return res.json({ success: false, err })
+    if(err) {
+      return res.json({ success: false, err })
+    }
 
     return res.status(200).json({ success: true });
   });
--- models/User.js
+++ models/User.js
@@ -1,25 +1,28 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    maxlength: 50
+    maxlength: 50,
   },
   email: {
     type: String,
-    trim: true,  // 빈공간 없애기
-    unique: 1
+    trim: true, // 빈공간 없애기
+    unique: 1,
   },
   password: {
     type: String,
-    minlength: 5
+    minlength: 5,
   },
   lastname: {
     type: String,
-    maxlength: 50
+    maxlength: 50,
   },
   role: {
     type: Number,
-    default: 0
+    default: 0,
   },
   image: String,
   token: {
@@ -27,7 +30,29 @@ const userSchema = mongoose.Schema({
   },
   tokenExp: {
     type: Number,
-  }
+  },
+});
+
+// 화살표 함수를 썼더니 this가 인식을 못했다.. 왜?
+// Arrow functions explicitly prevent binding this, 
+// so your method will not have access to the document and the above examples will not work. )
+userSchema.pre('save', function(next) {
+  let user = this;
+  console.log(user)
+
+  // password 가 변환 돨때만 암호화
+  if (user.isModified('password')) {
+    // 비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+  
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  };
 });
 
 const User = mongoose.model('User', userSchema);
